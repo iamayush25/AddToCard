10ec0f6d995f1c7c6e94cc8ea84b7da58a39706f
iamayush25 <ayush22@navgurukul.org>
2024-03-07 23:07:15 +0530

    update item quantity in cart

--- src/MyCarts/Home.js
+++ src/MyCarts/Home.js
@@ -6,12 +6,31 @@ function Home() {
     const [data, setData] = useState([])
     const [itemData, setitemData] = useState([])
 
-
+                     //  Adding Items into cart 
     const addItem = (item) => {
-        localStorage.setItem('data' , JSON.stringify([...itemData , { title: item.title, image: item.image, price: item.price , itemQuantity : 1 }]))
-        setitemData(JSON.parse(localStorage.getItem('data')))
-    }
+        let storedData = JSON.parse(localStorage.getItem('data')) || [];
+    
+                    // Check item is already Available in the cart ..........
+        const existingItemIndex = storedData.findIndex((storedItem) => storedItem.title === item.title);
+        if (existingItemIndex !== -1) {
+                 // If item already available in the cart , update item quantity......
+            storedData[existingItemIndex].price *= storedData[existingItemIndex].itemQuantity
+            storedData[existingItemIndex].itemQuantity += 1;
+        } else {
+                  // If item doesn't exist, add it to the cart......
+            storedData.push({ title: item.title, image: item.image, price: item.price, itemQuantity: 1 });
+        }
+    
+                 // Update localStorage .......
+        localStorage.setItem('data', JSON.stringify(storedData));
+    
+        // Update state
+        setitemData(storedData);
+    };
+    
 
+
+        // Fetch items fron the API ......
     useEffect(() => {
         const GetItem = async () => {
             let response = await fetch("https://fakestoreapi.com/products")
@@ -22,17 +41,18 @@ function Home() {
         GetItem()
     }, [])
 
+        // Display all the items...
     const DisplayItem = () => {
         return (
             <div className='main'>
                 {data.map((item, index) => {
                     return (
                         <div className="card" style={{ width: "18rem" }} key={item.id}>
-                            <h3 className="card-text">{item.category}</h3>
+                            <h3 className="card-text">{item.category.toUpperCase()}</h3>
                             <img src={`${item.image}`} className="card-img-top" />
                             <h5 className="card-title">{item.title}</h5>
-                            <p className="card-text">Price : {item.price}</p>
-                            <button onClick={() => addItem(item)} className="btn btn-primary" >ADD TO CART</button>
+                            <p className="card-text">Price : {item.price} $</p>
+                            <button onClick={() => addItem(item , index)} className="btn btn-primary" >ADD TO CART</button>
                         </div>
                     )
                 })}
