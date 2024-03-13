5ff88c0c774fc1b50b62e02473148271ef629894
iamayush25 <ayush22@navgurukul.org>
2024-03-13 22:50:44 +0530

    Update

--- src/MyCarts/Home.js
+++ src/MyCarts/Home.js
@@ -1,36 +1,35 @@
 import React, { useEffect, useState } from 'react'
 import './style.css'
 import { json } from 'react-router-dom'
+import Navbar from './Navbar'
 
 function Home() {
     const [data, setData] = useState([])
     const [itemData, setitemData] = useState([])
 
-                //  Adding Items into cart 
+    //  Adding Items into cart 
     const addItem = (item) => {
-        let storedData = JSON.parse(localStorage.getItem('data')) || [] ;
-    
-                // Check item is already Available in the cart ..........
+        let storedData = JSON.parse(localStorage.getItem('data')) || [];
+
+        // Check item is already Available in the cart ..........
         const existingItemIndex = storedData.findIndex((storedItem) => storedItem.title === item.title);
         if (existingItemIndex !== -1) {
-                // If item already available in the cart , update item quantity......
+            // If item already available in the cart , update item quantity......
             storedData[existingItemIndex].price *= storedData[existingItemIndex].itemQuantity
-            storedData[existingItemIndex].itemQuantity += 1 ; 
+            storedData[existingItemIndex].itemQuantity += 1;
         } else {
-                // If item doesn't exist, add it to the cart......
+            // If item doesn't exist, add it to the cart......
             storedData.push({ title: item.title, image: item.image, price: item.price, itemQuantity: 1 });
         }
-    
-                // Update localStorage .......
+
+        // Update localStorage .......
         localStorage.setItem('data', JSON.stringify(storedData));
-    
+
         // Update state
         setitemData(storedData);
     };
-    
 
-
-        // Fetch items fron the API ......
+    // Fetch items fron the API ......
     useEffect(() => {
         const GetItem = async () => {
             let response = await fetch("https://fakestoreapi.com/products")
@@ -41,31 +40,23 @@ function Home() {
         GetItem()
     }, [])
 
-        // Display all the items...
-    const DisplayItem = () => {
-        return (
-            <div className='main'>
-                {data.map((item, index) => {
-                    return (
-                        <div className="card" style={{ width: "18rem" }} key={item.id}>
-                            <h3 className="card-text">{item.category.toUpperCase()}</h3>
-                            <img src={`${item.image}`} className="card-img-top" />
-                            <h5 className="card-title">{item.title}</h5>
-                            <p className="card-text">Price : {item.price} $</p>
-                            <button onClick={() => addItem(item , index)} className="btn btn-primary" >ADD TO CART</button>
-                        </div>
+    // Display all the items...
+    return (
+        <div className='main'>
+            {data.map((item, index) => {
+                return (
+                    <div className="card" style={{ width: "18rem" }} key={item.id}>
+                        <h3 className="card-text">{item.category.toUpperCase()}</h3>
+                        <img src={`${item.image}`} className="card-img-top" />
+                        <h5 className="card-title">{item.title}</h5>
+                        <p className="card-text">Price : {item.price} $</p>
+                        <button onClick={() => addItem(item, index)} className="btn btn-primary" >ADD TO CART</button>
+                    </div>
                     )
                 })}
-            </div>
+                <Navbar item = {data}/>
+        </div>
         )
-    }
-
-    return (
-        DisplayItem()
-    )
-
-
-
 }
 
 export default Home;
