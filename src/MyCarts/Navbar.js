d883c286cd6f9abc8f7828bcd59f8899aba6e064
iamayush25 <ayush22@navgurukul.org>
2024-03-13 22:51:24 +0530

    Add Dropdown

--- src/MyCarts/Navbar.js
+++ src/MyCarts/Navbar.js
@@ -4,7 +4,7 @@ import Home from './Home';
 import AddToCart from './AddToCart';
 import './style.css'
 
-        // Navbar ......
+// Navbar ......
 function Navbar() {
     const [flag, setFlag] = useState("")
 
@@ -14,28 +14,35 @@ function Navbar() {
                 <h1>ShopCart</h1>
                 <span className='navItem'>
                     <span className='inputs'>
-                        <input className='inputBox' type='search' placeholder=' Search Item' />
+                        {/*<input className='inputBox' type='search' placeholder=' Search Item' />*/}
+                        <div>
+                            <datalist id="suggestions">
+                                <option>Second Option</option>
+                                <option>First option</option>
+                            </datalist>
+                            <input autoComplete="on" list="suggestions" />
+                        </div>
                         <button className='searchBtn'>Search</button>
                     </span>
-                        <ul className='links' >
-                            <li>
-                                <Link className='link' to="/">Home</Link>
-                            </li>
-                            <li>
-                                <Link className='link' to="/cart" ><i className="fa-solid fa-cart-shopping"></i></Link>
-                            </li>
-                        </ul>
+                    <ul className='links' >
+                        <li>
+                            <Link className='link' to="/">Home</Link>
+                        </li>
+                        <li>
+                            <Link className='link' to="/cart" ><i className="fa-solid fa-cart-shopping"></i></Link>
+                        </li>
+                    </ul>
 
                 </span>
 
             </header>
             <Routes>
                 <Route path='/' element={<Home />} />
-                <Route path='cart' element={<AddToCart/>} />
+                <Route path='cart' element={<AddToCart />} />
             </Routes>
         </div>
     )
-    
+
 }
 
 export default Navbar
