b99192c30e7570d22269db628609ad438deca52f
iamayush25 <ayush22@navgurukul.org>
2024-03-20 22:17:55 +0530

    Adjust card Item

--- src/MyCarts/AddToCart.js
+++ src/MyCarts/AddToCart.js
@@ -1,71 +1,67 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react';
+import './style.css';
 
 function AddToCart() {
+  const [cartData, setCartData] = useState([]);
+  const [price, setPrice] = useState([]);
 
-  const [cartData, setCartData] = useState([])
-  const [price, setPrice] = useState([])
+  const increaseQuantity = (item, index) => {
+    cartData[index].itemQuantity = item.itemQuantity + 1;
+    cartData[index].price = (item.price / (item.itemQuantity - 1)) * cartData[index].itemQuantity;
+    setCartData([...cartData]);
+    updateLocalStorage([...cartData]);
+  };
 
-        // increase Quantity of items ......
-    const increaseQuantity = (item, index) => {
-    cartData[index].itemQuantity = item.itemQuantity + 1
-    cartData[index].price = (item.price / (item.itemQuantity - 1)) * cartData[index].itemQuantity 
-    setCartData([...cartData])
-    updateLocalStorage([...cartData])
-    console.log(item.itemQuantity);
-    console.log(item.price);
-
-  }
-
-        // decrease Quantity of items .......
   const decreaseQuantity = (item, index) => {
     if (item.itemQuantity > 1) {
-      cartData[index].itemQuantity = item.itemQuantity - 1
-      cartData[index].price = price[0] * cartData[index].itemQuantity
-      setCartData([...cartData])
-      updateLocalStorage([...cartData])
-    
+      cartData[index].itemQuantity = item.itemQuantity - 1;
+      cartData[index].price = price[0] * cartData[index].itemQuantity;
+      setCartData([...cartData]);
+      updateLocalStorage([...cartData]);
+    } else if (item.itemQuantity === 1) {
+      cartData.splice(index, 1);
+      setCartData([...cartData]);
+      updateLocalStorage([...cartData]);
     }
+  };
 
-    else if (item.itemQuantity === 1) {
-      cartData.splice(index, 1)
-      setCartData([...cartData])
-      updateLocalStorage([...cartData])
-    }
-  }
-         // Update localStorage ......
   const updateLocalStorage = (data) => {
     localStorage.setItem('data', JSON.stringify(data));
   };
 
-  // useEffect hook for get cart data from localStorage
   useEffect(() => {
-    const data = JSON.parse(localStorage.getItem('data')) || []
-    setCartData(data)
-    const maping = data.map((product) => product.price)
-    setPrice(maping)
-    console.log(maping , "about");
-    
-  }, [])
+    const data = JSON.parse(localStorage.getItem('data')) || [];
+    setCartData(data);
+    const maping = data.map((product) => product.price);
+    setPrice(maping);
+  }, []);
 
-        // Display In cart .......
   return (
-    <div className='container'>
-      {cartData.map((item, index) => {
-        return (
+    <div className='cart-main-outer'>
+      <div className='cartHeading'><h1>Cart Items</h1></div>
+      <hr/>
+      <div className='container'>
+        {cartData.map((item, index) => (
           <div className="Addcard" key={index}>
-            <img src={`${item.image}`} className="card-img" />
+            <img src={`${item.image}`} className="card-img" alt={item.title} />
             <div className='card-Details'>
-              <h5 className="card-title">{item.title}</h5>
-              <p className="card-text">Price : {item.price} $</p>
-              <button onClick={() => decreaseQuantity(item, index)} className="quantity-btn">-</button>
-              <button className="quantity-btn">{item.itemQuantity}</button>
-              <button onClick={() => increaseQuantity(item, index)} className="quantity-btn">+</button>
+              <div className='cart-title'>
+                <h5 className="card-title">{item.title}</h5>
+              </div>
+              <div className='cart-price'>
+                <p className="card-text">Price: {item.price} $</p>
+                <div className="quantity-buttons">
+                  <button onClick={() => decreaseQuantity(item, index)} className="quantity-btn">-</button>
+                  <button className="quantity-display">{item.itemQuantity}</button>
+                  <button onClick={() => increaseQuantity(item, index)} className="quantity-btn">+</button>
+                </div>
+              </div>
             </div>
           </div>
-        )
-      })}
+        ))}
+      </div>
     </div>
-  )
+  );
 }
 
-export default AddToCart
+export default AddToCart;
