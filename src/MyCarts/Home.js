b2aec28945871bd636f21eb695db379600476629
iamayush25 <ayush22@navgurukul.org>
2024-03-21 12:17:24 +0530

    Update

--- src/MyCarts/Home.js
+++ src/MyCarts/Home.js
@@ -67,16 +67,6 @@ function Home() {
                 })}
             </div>
 
-            {/*Footer*/}
-
-            <div class="card mb-3" style={{width : "76em"}}>
-                <img src="..." className="card-img-top" alt="..."/>
-                    <div className="card-body">
-                        <h5 className="card-title">Card title</h5>
-                        <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                        <p className="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p>
-                    </div>
-            </div>
         </div>
     )
 }
