41108f898517c37712f6abf741588265eb2064ef
Ayush Sharma <ayush22@navgurukul.org>
2024-04-12 17:13:00 +0530

    update

--- src/MyCarts/Home.js
+++ src/MyCarts/Home.js
@@ -4,7 +4,7 @@ import { json } from 'react-router-dom'
 import AddToCart from './AddToCart'
 
 
-function Home() {
+function Home({filteredData}) {
     const [data, setData] = useState([])
     const [itemData, setitemData] = useState([])
     const [selectedCategory, setSelectedCategory] = useState('');
@@ -16,13 +16,7 @@ function Home() {
         // Check item is already available in the cart ..........
         const existingItemIndex = storedData.findIndex((storedItem) => storedItem.title === item.title);
         if (existingItemIndex !== -1) {
-            // storedData[existingItemIndex].itemQuantity += 1;
-            // storedData[existingItemIndex].price = (storedData[existingItemIndex].price / (storedData[existingItemIndex].itemQuantity - 1)) * storedData[existingItemIndex].itemQuantity;
-
-
-            // If item already available in the cart, remove it........
-            storedData.splice(existingItemIndex, 1);
-
+            alert("This product is already exist in the cart, if you want to increase it's quantity please go to cart.")
         } else {
             // If item doesn't exist, add it to the cart......
             storedData.push({ title: item.title, image: item.image, price: item.price, itemQuantity: 1 });
@@ -41,18 +35,10 @@ function Home() {
         const GetItem = async () => {
             let response = await fetch("https://fakestoreapi.com/products")
             const data = await response.json();
-            // console.log(data);
             setData(data)
         }
         GetItem()
     }, [])
-
-    // for filter 
-    const filterItemsByCategory = (category) => {
-        setSelectedCategory(category);
-    };
-
-    const filteredData = selectedCategory ? data.filter(item => item.category.toLowerCase() === selectedCategory.toLowerCase()) : data;
 
     // Display all the items...
     return (
@@ -63,19 +49,8 @@ function Home() {
             <hr />
             <span className='letestHeading'><h1>Letest Products</h1></span>
             <hr />
-            <div className='categoryFilter'>
-                <label htmlFor="categorySelect">Select Category:</label>
-                <select id="categorySelect" onChange={(e) => filterItemsByCategory(e.target.value)}>
-                    <option value="">All Categories</option>
-                    {/* Extract all unique categories from data */}
-                    {[...new Set(data.map(item => item.category.toLowerCase()))].map((category, index) => (
-                        <option key={index} value={category}>{category}</option>
-                    ))}
-                </select>
-            </div>
             <div className='main'>
                 {filteredData.map((item, index) => {
-                    const isItemInCart = itemData.some(cartItem => cartItem.title === item.title);
                     return (
                         <div className="card" style={{ width: "18rem" }} key={item.id}>
                             <h2 className="card-text">{item.category.toUpperCase()}</h2>
@@ -84,9 +59,7 @@ function Home() {
                             <hr />
                             <h5 className="card-title">{item.title}</h5>
                             <p className="card-text">Price : {item.price} $</p>
-                            <button onClick={() => addItem(item)} className="btn btn-primary" >
-                                {itemData.some(cartItem => cartItem.title === item.title) ? "Remove Item" : "ADD TO CART"}
-                            </button>
+                            <button onClick={() => addItem(item)} className="btn btn-primary" >ADD TO CART</button>
 
                         </div>
                     )
